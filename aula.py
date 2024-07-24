499a92b72e569f8ad9a63f2e0c871c37ad0b0b4f
Kirink212 <lfernandotexbicalho@hotmail.com>
2024-07-24 20:56:06 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,7 +1,7 @@
 # NÃO MEXER SENÃO QUEBRA EM PRODUÇÃO, POR FAVOR!
 def greetings_msg(nome):
     # TODO: Trocar isso aqui antes de mandar pra produção
-    return f"Koe rapaziada, {nome}. Koe rapaziada! KOEEEEEEEEEEEEEE, RA PA ZIA DAAAAAAA!"
+    return f"Koe, {nome}. Koe rapaziada! KOEEEEEEEEEEEEEE, RA PA ZIA DAAAAAAA!"
 
 greetings_msg("Luís Fernando Teixeira Bicalho")
 greetings_msg("Rodrigo Araújo Viannini")
